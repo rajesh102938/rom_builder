de751733904bff82bb07a65fc55a877dee9b14b1
rajesh102938 <77498110+rajesh102938@users.noreply.github.com>
2021-01-15 23:05:18 +0530

    Update clone.sh

--- clone.sh
+++ clone.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-
+git clone https://github.com/geopd/device_xiaomi_sakura -b Octavi-11 device/xiaomi/sakura
+git clone https://github.com/geopd/vendor_xiaomi_sakura -b lineage-18.0 vendor/xiaomi
+git clone https://github.com/darkhz/revvz_sakura kernel/xiaomi/msm8953
 # write commands if you want to clone some repos
